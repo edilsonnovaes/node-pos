5e1682d87f6bcb728c8862729f248941bc39d335
edilsonnovaes <edilson.novaesjr@gmail.com>
2020-09-12 15:08:27 -0300

    01 - routes and models

---
+++ routes/index-route.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const { route } = require('./product-route');
+const router = express.Router();
+
+//Middleware
+router.use(function(req, res, next){
+  console.log('Interceptação pelo middleware'); // LOG, autenticação, validação ...
+  next();
+});
+
+router.get('/', (req, res) => res.send("rota teste ok"));
+
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+//Definir porta onde o server vai responder
+const port = process.env.PORT || 3000;
+
+//Persistência
+const connectionString = "mongodb+srv://<usuario>:<senha>@cluster0-nffnp.mongodb.net/dbpos?retryWrites=true&w=majority";
+mongoose.connect(connectionString, {
+                                      useNewUrlParser:true, 
+                                      useUnifiedTopology: true, 
+                                      useFindAndModify: false
+                                    }
+);
+
+//Definindo as rotas
+const router = express.Router(); // Interceptar todas as rotas
+const productRoute = require('./routes/product-route');
+const indexRoute = require('./routes/index-route');
+
+// '/api' é o caminho padrão para as APIs REST
+app.use('/api', indexRoute);
+
+// Rota para produto
+app.use('/api/produtos/', productRoute);
+
+app.listen(port, () => {
+  console.log('server is up and running ... ');
+});
